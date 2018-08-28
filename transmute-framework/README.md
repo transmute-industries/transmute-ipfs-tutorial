bf6ef333d095c172bcec56a65a484450ca5896ff
Orie Steele <orie@transmute.industries>
2018-08-28 16:19:24 -0500

    update readme

--- transmute-framework/README.md
+++ transmute-framework/README.md
@@ -95,4 +95,100 @@ assert.deepEqual(streamModel.state.model, {
 });
 ```
 
-See the test directory for more detail.
+See the test directory for more detail, the end result of truffle test should be:
+
+```
+› truffle test
+Compiling ./contracts/AppStore.sol...
+Compiling ./contracts/AppStoreFactory.sol...
+Compiling ./contracts/Migrations.sol...
+Compiling transmute-framework/contracts/EventStore.sol...
+Compiling transmute-framework/contracts/EventStoreFactory.sol...
+Compiling transmute-framework/contracts/EventStoreLib.sol...
+
+
+  Contract: AppStore
+    ✓ constructor works
+    ✓ works with the transmute-framework (1241ms)
+
+
+  2 passing (1s)
+
+```
+
+If you encounter errors, it can be useful to find out which process is taking up a port:
+
+```
+lsof -i :8545
+lsof -i :5001
+```
+
+Here is an example of what an event looks like:
+
+```
+{
+  "event": {
+    "sender": "0x6e2c2ead78479b27f6c8e3ff0b9318722b99275a",
+    "key": {
+      "type": "EVENT_WRITTEN"
+    },
+    "value": {
+      "message": "so does redux!"
+    }
+  },
+  "meta": {
+    "tx": "0xfa8b8c2d1649784e269e68195d82fea97e2e38ba5eb7419e8749cea73d6a508a",
+    "ipfs": {
+      "key": "QmSohXeDaDzW5rJadVYHzbVxiVqqT4hRzUa5TJujZV2kcg",
+      "value": "QmaJmJo8Heqz8KQebtLmXP2sDvueLRWvCTKKwYWGXjcqVG"
+    },
+    "bytes32": {
+      "key": "0x425c9e1f3535c3976fa77f5390fb7a7dc91ad96d148dc471edffc8feb42bde89",
+      "value": "0xb1d08abfa2b502468b653e7c9b052f17ee5e7a55f2833d725a4c96d98e1fbf3f"
+    },
+    "receipt": {
+      "transactionHash": "0xfa8b8c2d1649784e269e68195d82fea97e2e38ba5eb7419e8749cea73d6a508a",
+      "transactionIndex": 0,
+      "blockHash": "0xfb5f04858774c0f137e8404ef2f9d00a60f509e20ddb215877425d223c36f028",
+      "blockNumber": 55,
+      "gasUsed": 116247,
+      "cumulativeGasUsed": 116247,
+      "contractAddress": null,
+      "logs": [
+        {
+          "logIndex": 0,
+          "transactionIndex": 0,
+          "transactionHash": "0xfa8b8c2d1649784e269e68195d82fea97e2e38ba5eb7419e8749cea73d6a508a",
+          "blockHash": "0xfb5f04858774c0f137e8404ef2f9d00a60f509e20ddb215877425d223c36f028",
+          "blockNumber": 55,
+          "address": "0xc36d57337e31991b481d7e8fdf71f337f4ea3f25",
+          "data": "0x00000000000000000000000000000000000000000000000000000000000000020000000000000000000000006e2c2ead78479b27f6c8e3ff0b9318722b99275a425c9e1f3535c3976fa77f5390fb7a7dc91ad96d148dc471edffc8feb42bde89b1d08abfa2b502468b653e7c9b052f17ee5e7a55f2833d725a4c96d98e1fbf3f",
+          "topics": [
+            "0x4c7900e30963aa4aed068e34036112a44f45ef40623e8892337b84316e5fcb64"
+          ],
+          "type": "mined"
+        }
+      ],
+      "status": "0x01",
+      "logsBloom": "0x00000000000000020000000000000000000000000000000000000000000080000000000000000000000000000008000000000000000000000000000000000004800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000"
+    }
+  }
+}
+```
+
+
+An example of a stream model is:
+
+```
+{
+  "contractAddress": "0x90e8521dc5b8d4166342e88989c80163df26651f",
+  "model": {
+    "messages": [
+      "hello world",
+      "event sourcing rocks!",
+      "so does redux!"
+    ]
+  },
+  "lastIndex": 2
+}
+```
--- transmute-framework/test/AppStore.spec.js
+++ transmute-framework/test/AppStore.spec.js
@@ -14,6 +14,8 @@ const transmuteConfig = {
 
 const filter = event => {
   // process all events
+  // return event.key.type === 'Patient';
+  // return event !== null && event !== undefined && typeof event === 'object';
   return event;
 };
 
@@ -23,6 +25,7 @@ const reducer = (state, event) => {
     case 'EVENT_WRITTEN': {
       return {
         ...state,
+        // accumulate all messages in state.messages property
         messages: (state.messages || []).concat(event.value.message)
       };
     }
@@ -32,7 +35,11 @@ const reducer = (state, event) => {
   }
 };
 
-// like describe but for smart contracts
+// Like describe but for smart contracts
+// gives a new instance of the contract deployed to the ethereum test network via truffle
+// useful for testing smart contracts.
+// provides web3 accounts with test ETH
+// By default contracts are deployed from accounts[0].
 contract('AppStore', accounts => {
   it('constructor works', async () => {
     const storage = await AppStore.deployed();
@@ -40,14 +47,21 @@ contract('AppStore', accounts => {
   });
 
   it('works with the transmute-framework', async () => {
+    // This instance provides an Event Sourced interface to Ethereum and IPFS
     const eventStore = new T.EventStore({
       eventStoreArtifact: AppStore,
       ...transmuteConfig
     });
 
+    // It must be initialized before being called, this confirms it is deployed to the network.
     await eventStore.init();
+
+    // This instance uses common redux functionality to accumulate state from events stored on Etheruem and IPFS.
     const streamModel = new T.StreamModel(eventStore, filter, reducer, null);
+
+    // Events are downloaded from Ethereum and IPFS and processed in the streamModel state.
     await streamModel.sync();
+
     // the model starts empty
     assert.deepEqual(streamModel.state.model, {});
 
@@ -61,15 +75,19 @@ contract('AppStore', accounts => {
         message: 'hello world'
       }
     );
+
+    // meta data contains information about the ethereum transaction and ipfs hashes of the key and value objects.
     assert(typeof receipt.meta !== undefined);
 
     // syncing updates the model state
     await streamModel.sync();
 
+    // Now the model state has been updated, as an event exists and has been processed.
     assert.deepEqual(streamModel.state.model, {
       messages: ['hello world']
     });
 
+    // Here we write a new event.
     await eventStore.write(
       accounts[0],
       {
@@ -79,7 +97,9 @@ contract('AppStore', accounts => {
         message: 'event sourcing rocks!'
       }
     );
-    await eventStore.write(
+
+    // And another new event.
+    let event = await eventStore.write(
       accounts[0],
       {
         type: 'EVENT_WRITTEN'
@@ -88,9 +108,14 @@ contract('AppStore', accounts => {
         message: 'so does redux!'
       }
     );
+
+    // console.log(JSON.stringify(event, null, 2))
+
     // calling sync when no changes have occurred does nothing.
     await streamModel.sync();
     await streamModel.sync();
+    
+    // calling sync does not change the model state if no new events exist, or the reducer does not handle them.
     assert.deepEqual(streamModel.state.model, {
       messages: ['hello world', 'event sourcing rocks!', 'so does redux!']
     });
@@ -98,5 +123,14 @@ contract('AppStore', accounts => {
     assert.deepEqual(streamModel.state.model, {
       messages: ['hello world', 'event sourcing rocks!', 'so does redux!']
     });
+
+    // Now we can save this model state to a JSON / NoSQL database, and perform queries or 
+    // analysis on objects which are built from an immutable audit log.
+    // ... db.save(key, streamModel.state.model)
+
+    // console.log(JSON.stringify(streamModel.state, null, 2))
+  
+
+
   });
 });
